1679341224ab7c68dee78336e2da17b35c695e2c
Vu Duy Tuan Anh <vuduytuananh@Vus-MacBook-Air.local>
2016-11-21 15:53:54 +0100

    first commit

--- models/admin.js
+++ models/admin.js
@@ -1,3 +1,5 @@
+//test purpose
+//this data will be replace by using process.ADMINNAME and process.PASSWORD at production time
 module.exports = {
   id: "123asfd1DDXX124HIQxyriuYIR3",
   usertype : "admin",
